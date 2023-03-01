4e8c21929eee0989783e7f192e7685bce5d9e890
lyn <dldudsh83@gmail.com>
2023-03-01 21:48:50 +0900

    백엔드(node.js)에서 qutd 요청 하여 cors 해결_4

--- src/components/views/desktop/test3/DsTest3.jsx
+++ src/components/views/desktop/test3/DsTest3.jsx
@@ -2,6 +2,7 @@ import { async } from "@firebase/util";
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import instance from "../../../../axios";
 import quotesInstance from "../../../../quotesInstance";
 import { turnMenu } from "../../../../redux/_actions/turn_action";
 
@@ -9,8 +10,11 @@ function DsTest3() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(turnMenu("TEST3_MENU"));
-    quotesInstance.get("/api/qotd").then((data) => {
-      console.log(data);
+    // quotesInstance.get("/api/qotd").then((data) => {
+    //   console.log(data);
+    // });
+    instance.get("/api/qotds/qotd").then((res) => {
+      console.log("qotds", res);
     });
   }, []);
 
