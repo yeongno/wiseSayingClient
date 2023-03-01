2ddbc0906a39a4f1092a86fa89f4dda0fd2f4e41
lyn <dldudsh83@gmail.com>
2023-03-01 21:09:28 +0900

    배포하면 cors 문제 때문에 netlify Functions를 이용하여 일단 세팅_!

---
+++ src/netlifyFunction.js
@@ -0,0 +1,17 @@
+// netlify/functions/qotd.js
+
+const { default: quotesInstance } = require("./quotesInstance");
+
+exports.handler = async (event, context) => {
+  const apiUrl = "https://favqs.com/api/qotd";
+  const response = await quotesInstance.get(apiUrl);
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+    body: JSON.stringify(response.data),
+  };
+};
